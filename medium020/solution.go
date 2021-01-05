82314a9ee7356ee681f3bd676b0023120d8be16c
naoketa <49198895+naoketa@users.noreply.github.com>
2021-01-06 00:33:11 +0900

    繰り返し2乗法でO(logN)にする

--- medium020/solution.go
+++ medium020/solution.go
@@ -1,14 +1,16 @@
 package medium020
 
 func myPow(x float64, n int) float64 {
-	switch {
-	case n == 0:
+	if n == 0 {
 		return 1
-	case n > 0:
-		return x * myPow(x, n-1)
-	case n < 0:
-		return 1 / x * myPow(x, n+1)
-	default:
-		return -1
 	}
+	if n%2 == 0 {
+		d := myPow(x, n/2)
+		return d * d
+	}
+	if n > 0 {
+		return x * myPow(x, n-1)
+	}
+	return 1 / x * myPow(x, n+1)
+
 }
