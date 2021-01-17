b4d41da0c7374b029816013f028380992edd6e0c
naoketa <49198895+naoketa@users.noreply.github.com>
2021-01-18 00:11:01 +0900

    XORで空間計算量O(1)

--- easy136/solution.go
+++ easy136/solution.go
@@ -1,18 +1,9 @@
 package easy136
 
 func singleNumber(nums []int) int {
-	numToCnt := make(map[int]int)
+	var ans int
 	for _, v := range nums {
-		if _, ok := numToCnt[v]; ok {
-			numToCnt[v]++
-		} else {
-			numToCnt[v] = 1
-		}
+		ans ^= v
 	}
-	for k, v := range numToCnt {
-		if v < 2 {
-			return k
-		}
-	}
-	return -1
+	return ans
 }
