48e72e8d19032007c4de3471b1b87742f98491fa
naoketa <49198895+naoketa@users.noreply.github.com>
2021-01-17 10:58:23 +0900

    解いた

---
+++ easy543/solution.go
@@ -0,0 +1,37 @@
+package easy543
+
+import (
+	"fmt"
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	ans := -1
+	var search func(root *TreeNode) int
+	search = func(root *TreeNode) int {
+		if root == nil {
+			return 0
+		}
+		left := search(root.Left)
+		right := search(root.Right)
+		fmt.Printf("left:%d\n", left)
+		fmt.Printf("right:%d\n", right)
+		ans = max(ans, (left + right + 1))
+		return max(left, right) + 1
+	}
+	search(root)
+	return ans - 1
+}
+
+func max(a, b int) int {
+	return int(math.Max(float64(a), float64(b)))
+}
