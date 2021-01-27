9733c7ffd5da92f092cc0fc2573f4b1807b0c1fc
naoketa <49198895+naoketa@users.noreply.github.com>
2021-01-28 02:03:13 +0900

    O(n)

--- medium338/solution.go
+++ medium338/solution.go
@@ -2,16 +2,8 @@ package medium338
 
 func countBits(num int) []int {
 	ans := make([]int, num+1)
-	for i := range ans {
-		idx := i
-		sum := 0
-		for i != 0 {
-			if i%2 == 1 {
-				sum++
-			}
-			i = (i - i%2) / 2
-		}
-		ans[idx] = sum
+	for i := 1; i <= num; i++ {
+		ans[i] = ans[i&(i-1)] + 1
 	}
 	return ans
 }
